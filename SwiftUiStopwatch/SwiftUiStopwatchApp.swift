c9aa301459b12c2472a88edc0767930273f33bfc
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-08-29 11:33:07 +0600

    Initial Commit

---
+++ SwiftUiStopwatch/SwiftUiStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStopwatchApp.swift
+//  SwiftUiStopwatch
+//
+//  Created by Auto on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
